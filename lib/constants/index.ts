50b24dd4395a2a86a67cc51aafe7d7fb339718ee
Andre Chretien <princeandre@hotmail.fr>
2025-03-19 09:26:58 -0400

    sign up page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -14,3 +14,9 @@ export const signInDefaultValues = {
   email: "",
   password: "",
 };
+export const signUpDefaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
