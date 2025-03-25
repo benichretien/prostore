6a9cd44c83cec9b0dc6fbd17215642281df48587
Andre Chretien <princeandre@hotmail.fr>
2025-03-25 10:42:46 -0400

    jwt name & role

--- auth.ts
+++ auth.ts
@@ -53,12 +53,33 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       //set the user id from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
+
       //if there is an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
       }
 
       return session;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user, trigger, session }: any) {
+      //Assign user fields to token
+      if (user) {
+        token.role = user.role;
+        // if user has no name then use the email
+        if (user.name === "NO_EMAIL") {
+          token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
