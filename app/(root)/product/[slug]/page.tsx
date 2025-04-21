b4376643d64d2bf02c19009dd4871ad9b09d146b
Andre Chretien <princeandre@hotmail.fr>
2025-04-21 18:44:02 -0400

    auth update

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,11 +1,11 @@
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+
 import { Card, CardContent } from "@/components/ui/card";
 import ProductPrice from "@/components/shared/product/product-price";
 import ProductImages from "@/components/shared/product/product-images";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
-
+import AddToCart from "@/components/shared/product/add-to-cart";
 interface Props {
   params: Promise<{ slug: string }>;
 }
@@ -64,9 +64,16 @@ const ProductDetailsPage = async ({ params }: Props) => {
                 </div>
                 {product.stock > 0 && (
                   <div className="flex-center">
-                    <Button className="w-full cursor-pointer">
-                      Add To Cart
-                    </Button>
+                    <AddToCart
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        price: product.price,
+                        qty: 1,
+                        image: product.images![0],
+                      }}
+                    />
                   </div>
                 )}
               </CardContent>
--- auth.ts
+++ auth.ts
@@ -3,7 +3,8 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 import { compareSync } from "bcrypt-ts-edge";
-import type { NextAuthConfig } from "next-auth";
+// import { cookies } from "next/headers";
+import { authConfig } from "./auth.config";
 
 export const config = {
   pages: {
@@ -11,7 +12,7 @@ export const config = {
     error: "signIn",
   },
   session: {
-    strategy: "jwt",
+    strategy: "jwt" as const,
     maxAge: 30 * 24 * 60 * 60, //30days
   },
   adapter: PrismaAdapter(prisma),
@@ -48,40 +49,53 @@ export const config = {
       },
     }),
   ],
-  callbacks: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    async session({ session, user, trigger, token }: any) {
-      //set the user id from the token
-      session.user.id = token.sub;
-      session.user.role = token.role;
-      session.user.name = token.name;
+  ...authConfig.callbacks,
+  // callbacks: {
+  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //   async session({ session, user, trigger, token }: any) {
+  //     //set the user id from the token
+  //     session.user.id = token.sub;
+  //     session.user.role = token.role;
+  //     session.user.name = token.name;
 
-      //if there is an update, set the user name
-      if (trigger === "update") {
-        session.user.name = user.name;
-      }
+  //     //if there is an update, set the user name
+  //     if (trigger === "update") {
+  //       session.user.name = user.name;
+  //     }
 
-      return session;
-    },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    async jwt({ token, user, trigger, session }: any) {
-      //Assign user fields to token
-      if (user) {
-        token.role = user.role;
-        // if user has no name then use the email
-        if (user.name === "NO_EMAIL") {
-          token.name = user.email!.split("@")[0];
+  //     return session;
+  //   },
+  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //   async jwt({ token, user, trigger, session }: any) {
+  //     //Assign user fields to token
+  //     if (user) {
+  //       token.role = user.role;
+  //       // if user has no name then use the email
+  //       if (user.name === "NO_EMAIL") {
+  //         token.name = user.email!.split("@")[0];
 
-          // Update database to reflect the token name
-          await prisma.user.update({
-            where: { id: user.id },
-            data: { name: token.name },
-          });
-        }
-      }
-      return token;
-    },
-  },
-} satisfies NextAuthConfig;
+  //         // Update database to reflect the token name
+  //         await prisma.user.update({
+  //           where: { id: user.id },
+  //           data: { name: token.name },
+  //         });
+  //       }
+  //     }
+  //     return token;
+  //   },
+  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //   authorized({ request, auth }: any) {
+  //     //Check for session cart cookie
+  //     if (!request.cookies.get("sessionCartId")) {
+  //       //generate new session cart id cookie
+  //       const sessionCartId = crypto.randomUUID();
+  //       console.log(sessionCartId);
+  //       return true;
+  //     } else {
+  //       return true;
+  //     }
+  //   },
+  // },
+};
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
